918a3bc6b5c90e09118977ad8846340dec9b739d
Rubel Hossain <rubrex96@hotmail.com>
2022-11-27 16:02:25 +0600

    all buyers added

--- src/components/Dashboard/AllBuyers/AllBuyers.jsx
+++ src/components/Dashboard/AllBuyers/AllBuyers.jsx
@@ -1,9 +1,92 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Table } from "flowbite-react";
 import React from "react";
+import Loading from "../../Shared/Loading/Loading";
+import PrimaryButton from "../../Shared/PrimaryButton/PrimaryButton";
 
 const AllBuyers = () => {
+  const {
+    data: allBuyers,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["allBuyers"],
+    queryFn: async () => {
+      const allBuyerUrl = import.meta.env.VITE_API + "/users/buyers";
+      const res = await axios.get(allBuyerUrl);
+      return res.data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  // handle delete
+  const handleDelete = (email) => {
+    // works the same for buyers/users role
+    const deleteUrl = import.meta.env.VITE_API + "/users/sellers/" + email;
+    axios.delete(deleteUrl).then((response) => {
+      if (response.data.deletedCount) {
+        console.log(response.data);
+        refetch();
+      }
+    });
+  };
+
   return (
-    <div>
-      <h2 className="text-2xl">All Buyers</h2>
+    <div className="p-2 md:p-5">
+      <h2 className="text-xl  mb-10 pl-2 border-l-4 border-primary ">
+        Buyers List
+      </h2>
+
+      {/* Table */}
+      <Table hoverable={true}>
+        <Table.Head>
+          <Table.HeadCell>
+            <span className="sr-only">Index</span>
+          </Table.HeadCell>
+          <Table.HeadCell>Buyer name</Table.HeadCell>
+          <Table.HeadCell>Email</Table.HeadCell>
+          <Table.HeadCell>Delete</Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          {allBuyers.map((buyer, index) => {
+            return (
+              <Table.Row
+                key={buyer._id}
+                className="bg-white dark:border-gray-700 dark:bg-gray-800"
+              >
+                <Table.Cell>
+                  <div
+                    className="delete-button"
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          "Are you sure you wish to delete this item?"
+                        )
+                      )
+                        this.onCancel(item);
+                    }}
+                  />
+                  {++index}
+                </Table.Cell>
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {buyer.name}
+                </Table.Cell>
+                <Table.Cell>{buyer.email}</Table.Cell>
+                <Table.Cell>
+                  <PrimaryButton onClick={() => handleDelete(buyer.email)}>
+                    X
+                  </PrimaryButton>
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+      {/* Table ends */}
     </div>
   );
 };
