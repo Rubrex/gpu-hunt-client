b46c82f86d2bca648af676d92b0b2fb41652e101
Rubel Hossain <rubrex96@hotmail.com>
2022-11-27 20:25:01 +0600

    Errorpage added

--- src/components/Shared/ErrorPage/ErrorPage.jsx
+++ src/components/Shared/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+import toast from "react-hot-toast";
+import { Link, useRouteError } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthProvider";
+import "./ErrorPage.css";
 
 const ErrorPage = () => {
-  return <div>Not Found</div>;
+  const { logOut } = useContext(AuthContext);
+
+  const error = useRouteError();
+  console.log(error);
+  const handleLogout = () => {
+    logOut()
+      .then(() => {
+        toast.success("Logged out");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <section className="page_404 mt-10">
+      <h1 className="text-3xl">Something Went Wrong</h1>
+
+      <section className="error-container">
+        <span className="four">
+          <span className="screen-reader-text">4</span>
+        </span>
+        <span className="zero">
+          <span className="screen-reader-text">0</span>
+        </span>
+        <span className="four">
+          <span className="screen-reader-text">4</span>
+        </span>
+      </section>
+      <p className="zoom-area">
+        <b className="text-xl">{error.statusText} </b>
+      </p>
+      <div className="link-container ">
+        <Link to="/" className="more-link bg-primary">
+          Visit Home Page
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
