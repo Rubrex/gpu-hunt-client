b60936c829bed6368fca821357fdd1b0d50cbce2
Rubel Hossain <rubrex96@hotmail.com>
2022-11-28 03:37:38 +0600

    .env.sample added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,6 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
+import Loading from "../Shared/Loading/Loading";
 
 const Blogs = () => {
+  const { data: blogs, isLoading } = useQuery({
+    queryKey: ["blogs"],
+    queryFn: async () =>
+      axios.get(`${import.meta.env.VITE_API}/blogs`).then((res) => res.data),
+  });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  console.log(blogs);
   return (
     <div>
       <h2 className="text-3xl">Blogs</h2>
